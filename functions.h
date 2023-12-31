94bab2d263d3ed8b6602224a171a83447c47f060
Daniilas <ledmanas@gmail.com>
2023-12-31 16:05:18 +0200

    lol

--- functions.h
+++ functions.h
@@ -3,15 +3,21 @@
 #define UNI_CPP_FUNCTIONS_H
 
 
-struct Studentas {
-    string vard, pav;
-    vector <int> paz;
-    int egz;
-    float rez;
+class Studentas {
+private:
+    std::string vardas_;
+    std::string pavarde_;
+    double egzaminas_;
+    std::vector<double> nd_;
 
-    string get_string();
+public:
+    Studentas() : egzaminas_(0) { }  // default konstruktorius
+    Studentas(std::istream& is);
+    inline std::string vardas() const { return vardas_; }    // get'eriai, inline
+    inline std::string pavarde() const { return pavarde_; }  // get'eriai, inline
+    double galBalas(double (*) (vector<double>) = mediana) const;  // get'eriai
+    std::istream& readStudent(std::istream&);  // set'eriai
 };
-
 list<Studentas> list_inputas();
 vector <Studentas> inputas();
 void printas(vector <Studentas>& sarasas);
